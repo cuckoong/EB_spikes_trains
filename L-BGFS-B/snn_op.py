ff3be1a92e191b9fe7f1d822a6f67ce6edc7abca
Pak <cuckoong@users.noreply.github.com>
2021-01-01 13:31:05 +0800

    Update snn_op.py

--- L-BGFS-B/snn_op.py
+++ L-BGFS-B/snn_op.py
@@ -1,11 +1,20 @@
-from models import SOD, POISSON, NBGLM, my_score_fun
+from models import SOD, POISSON, NBGLM
 from sklearn.model_selection import GridSearchCV, cross_validate
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import Pipeline
+
+
+def my_score_fun(clf, X, y):
+    x0 = clf['clf'].op.x
+    possitive_LLsum = -1 * clf['clf'].objective(x0, X, y)
+    return possitive_LLsum
+
 
 def my_mse_fun(clf, X, y):
-    x0 = clf.op.x
+    x0 = clf.name_steps.clf.op.x
     y_pred = clf.predict(X)
     mse = mean_squared_error(y, y_pred)
     return mse
@@ -15,46 +24,32 @@ if __name__ == '__main__':
     y_file = 'y.npy'
     X = np.load(X_file)
     y_all = np.load(y_file)
-    #LL_poisson_list = []
-    #mse_poisson_list = []
-    LL_sod_list =[]
-    mse_sod_list=[]
-    scores_fun = {'ll_f':my_score_fun,
-                  'mse_f':my_mse_fun}
+    LL_list = []
+    # mse_list = []
+    # scores_fun = {'ll_f':my_score_fun,
+    #               'mse_f':my_mse_fun}
 
     for i in range(y_all.shape[1]):
         y=y_all[:,i]
 
         lam = [0.1, 1, 10]
-        param_grid = dict(lam=lam)
-        # estimator of poisson
-        #grid search
-        '''
-        grid_poisson = GridSearchCV(estimator=POISSON(), param_grid=param_grid, n_jobs=15,
-                                    cv=5, verbose=10, scoring=my_score_fun)
-        grid_poisson_result = grid_poisson.fit(X, y)
-        print("Best: {0}, using {1}".format(grid_poisson_result.best_score_, grid_poisson_result.best_params_))
-        # cv
+        param_grid = dict(clf__lam=lam)
 
-        grid_poisson_cv = cross_validate(POISSON(lam=grid_poisson_result.best_params_), X, y, n_jobs=15,
-                                         scoring=scores_fun, cv=5)
-        LL_poisson_list.extend(grid_poisson_cv['test_ll_f'])
-        mse_poisson_list.extend(grid_poisson_cv['test_mse_f'])
-        '''
+        pipe = Pipeline([
+            ('scale', StandardScaler()),
+            ('clf', SOD())])
 
+        grid = GridSearchCV(pipe, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2, scoring=my_score_fun)
+        grid.fit(X, y)
 
-        # estimator of sod
-        #grid search
-        grid_sod = GridSearchCV(estimator=SOD(), param_grid=param_grid, n_jobs=15,
-                                    cv=5, verbose=10, scoring=my_score_fun)
-        grid_sod_result = grid_sod.fit(X, y)
-        print("Best: {0}, using {1}".format(grid_sod_result.best_score_, grid_sod_result.best_params_))
-        # cv
-        grid_sod_cv = cross_validate(SOD(lam=grid_sod_result.best_params_['lam']), X, y, n_jobs=15,
-                                         scoring=scores_fun, cv=5)
-        LL_sod_list.extend(grid_sod_cv['test_ll_f'])
-        mse_sod_list.extend(grid_sod_cv['test_mse_f'])
+        pipe_op = Pipeline([
+            ('scale', StandardScaler()),
+            ('clf', SOD(lam=grid.best_params_['clf__lam']))])
+        #cv
+        grid_cv = cross_validate(pipe_op, X, y, scoring=my_score_fun, cv=5, n_jobs=-1)
+        LL_list.extend(grid_cv['test_score'])
+        # mse_list.extend(grid_cv['test_scoring'])
 
-    df = dict(LL_sod = LL_sod_list, mse_sod=mse_sod_list)#, LL_poisson=LL_poisson_list, mse_poisson = mse_poisson_list)
+    df = dict(LL = LL_list)
     df = pd.DataFrame(df)
-    #df.to_csv('snn_sod_50sim_500bin_10neuron.csv')
+    df.to_csv('snn_sod_50sim_500bin_10neuron.csv')
